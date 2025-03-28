466d0663fe3829846c4e06d8af29243297854d3e
이상엽 <infightersy@naver.com>
2025-03-28 14:52:17 +0900

    로깅 필터 추가

---
+++ api-gateway/src/main/java/com/test/lsy/apigateway/config/GlobalLoggingFilter.java
@@ -0,0 +1,25 @@
+package com.test.lsy.apigateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalLoggingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        // 요청 URI 로깅
+        log.info("Request URI: {}", exchange.getRequest().getURI());
+
+        // 응답 완료 후 로깅
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            log.info("Response Status: {}", exchange.getResponse().getStatusCode());
+        }));
+    }
+}
