c5f2adae6893aa1d39a74fa21aa0143f73491a26
이상엽 <infightersy@naver.com>
2025-03-31 11:39:00 +0900

    ordered 추가

--- api-gateway/src/main/java/com/test/lsy/apigateway/config/GlobalLoggingFilter.java
+++ api-gateway/src/main/java/com/test/lsy/apigateway/config/GlobalLoggingFilter.java
@@ -3,13 +3,14 @@ package com.test.lsy.apigateway.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class GlobalLoggingFilter implements GlobalFilter {
+public class GlobalLoggingFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -21,5 +22,10 @@ public class GlobalLoggingFilter implements GlobalFilter {
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
             log.info("Response Status: {}", exchange.getResponse().getStatusCode());
         }));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
